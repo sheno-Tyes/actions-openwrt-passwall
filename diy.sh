db5e8bcfe66dcb8e2f614a39427b79511a45005a
sheno-Tyes <42202881+sheno-Tyes@users.noreply.github.com>
2020-04-18 15:52:33 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -10,7 +10,6 @@
 sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 mkdir package/base-files/files/config
 echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
-sed -i '5d' feeds.conf.default
 echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
 echo 'src-git TinyTitanPro https://github.com/TinyTitanPro/lienol-openwrt-package' >> feeds.conf.default
 ./scripts/feeds update -a && ./scripts/feeds install -a
